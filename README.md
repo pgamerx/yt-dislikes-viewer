af0f15bca89dbbfbd184c5b146b03060bb8a9903
PGamerX <websitedesigner123456@gmail.com>
2021-11-16 13:43:44 +0530

    Ready for Chrome Store

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A browser extension that allows you to see dislikes on a youtube video after the
 ## Important
 Hey there, I have just checked the Youtube's official change-logs and it seems that they will stop providing dislikes in the youtube Data API.
 That's why I am launching an archiving API that will allow anyone to see dislikes of any videos. This package will keep logging number of dislikes of any video and feed it to the API. In future, we will do something even better!
+
 ## Installation 
 ### For Chrome,Edge,Brave,Opera and any Chromium Browser 
 Currently I don't want to upload it to Chrome web Store because I will have to pay a 5$ fees and there is a high chance google will reject this extension.
--- popup/index.js
+++ popup/index.js
@@ -1,18 +1,29 @@
 function save_options() {
-    var api = document.getElementById('wptk').value;
-      chrome.storage.sync.set({
-      savedApi: api,
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
-    });
-  }
+  var api = document.getElementById('wptk').value;
+    chrome.storage.sync.set({
+    savedApi: api,
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
 
-console.log(document.getElementById('save'))
+function how(){
+window.open("https://blog.hubspot.com/website/how-to-get-youtube-api-key", '_blank').focus();
+
+}
+function discord(){
+window.open("https://u.pgamerx.com/donate", '_blank').focus();
+}
+function donate(){
+window.open("https://ko-fi.com/pgamerx", '_blank').focus();
+}
+
 document.getElementById('save').addEventListener('click', save_options);
-
-  
+document.getElementById('how').addEventListener('click', how);
+document.getElementById('discord').addEventListener('click', discord);
+document.getElementById('donate').addEventListener('click', donate);
