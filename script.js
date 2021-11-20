f98d83239e513e022b2391f3e50a19106f616b9f
Peipr <peipryt@gmail.com>
2021-11-20 06:45:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,13 +39,13 @@ chrome.storage.sync.get("savedApi", ({ savedApi }) => {
           await put_on_repl(video_id, parseInt(dislikeNo));
         });
         console
-          .log("Putting on Archieve API")
+          .log("Putting on Archive API")
           .catch((err) => console.error(err));
       } else {
         const disss = await fetch_from_repl(video_id);
         console.log(disss + " " + " disss ");
         editDislikes(disss);
-        console.log("Fetched from archieve API");
+        console.log("Fetched from archive API");
       }
     }
 
