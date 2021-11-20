4aa44a3b88b9f7c8c0bc4ce8d86ab58804153c1a
PGamerX <websitedesigner123456@gmail.com>
2021-11-20 08:31:38 +0530

    1.6.2

--- popup/index.js
+++ popup/index.js
@@ -1,29 +1,38 @@
-function save_options() {
-  var api = document.getElementById('wptk').value;
-    chrome.storage.sync.set({
-    savedApi: api,
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+function save_options(e) {
+  var api = document.getElementsByClassName("wptk")[0].value;
+  console.log(e);
+
+  if (api === "") {
+    var status = document.getElementsByClassName("status")[0];
+    status.textContent = "Please insert a valid API key.";
+    status.style.color = "#ff0000";
+
+    setTimeout(() => {
+      var status = document.getElementsByClassName("status")[0];
+      status.textContent = "";
+    }, 5000);
+  } else {
+    var status = document.getElementsByClassName("status")[0];
+    status.textContent = "Option saved.";
+    status.style.color = "#1eff00";
+    chrome.storage.sync.set(
+      {
+        savedApi: api,
+      },
+      function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementsByClassName("status")[0];
+        status.textContent = "Options saved.";
+
+        setTimeout(() => {
+          var status = document.getElementsByClassName("status")[0];
+          status.textContent = "";
+        }, 5000);
+      }
+    );
+  }
 }
 
-function how(){
-window.open("https://blog.hubspot.com/website/how-to-get-youtube-api-key", '_blank').focus();
-
-}
-function discord(){
-window.open("https://u.pgamerx.com/discord", '_blank').focus();
-}
-function donate(){
-window.open("https://ko-fi.com/pgamerx", '_blank').focus();
-}
-
-document.getElementById('save').addEventListener('click', save_options);
-document.getElementById('how').addEventListener('click', how);
-document.getElementById('discord').addEventListener('click', discord);
-document.getElementById('donate').addEventListener('click', donate);
+document
+  .getElementsByClassName("save")[0]
+  .addEventListener("click", save_options);
