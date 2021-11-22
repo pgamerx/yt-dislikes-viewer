bdfd85d73b7023980e3fcf18760c9669f8b4b316
PGamerX <websitedesigner123456@gmail.com>
2021-11-22 13:09:57 +0530

    reverting

--- service.js
+++ service.js
@@ -23,14 +23,3 @@ function executeScript(tabId) {
     file: "script.js",
   });
 }
-
-browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  // read changeInfo data and do something with it
-  // like send the new url to contentscripts.js
-  if (changeInfo.url) {
-    browser.tabs.sendMessage(tabId, {
-      message: "progressbar",
-      url: changeInfo.url,
-    });
-  }
-});
