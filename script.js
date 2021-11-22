502012b3647a876d06937d104d64e097dd6e0320
thereaper90917 <viper90917@gmail.com>
2021-11-23 01:05:44 +0200

    fixed ratio bar for 'thanks' button and when 'thanks' and 'clip' button appear together(previous one had a bug)

--- script.js
+++ script.js
@@ -118,27 +118,19 @@ chrome.storage.sync.get("savedApi", ({ savedApi }) => {
     }
 
     function addBar(likePercentage) {
-      // let progress = document.createElement("div");
-      // let parentNode = document.getElementById("menu-container");
-      // progress.setAttribute("class", "style-scope ytd-sentiment-bar-renderer");
-      // progress.setAttribute("id", "like-bar");
-
-      // parentNode.appendChild(progress);
-      //   const selector = document.querySelector(
-      //     "div#menu.style-scope.ytd-video-primary-info-renderer"
-      //   );
-
       const selector = document.getElementById("menu-container");
 
       const prgroess = document.getElementById("custom-progress");
 
-      const clipButton = document.querySelectorAll(
-        "yt-formatted-string#text.style-scope.ytd-button-renderer.style-default.size-default"
-      );
+      let clipButton = document.querySelector('[aria-label="Clip"]');
+      let ThanksButton = document.querySelector('[aria-label="Thanks"]');
+      // const clipButton = document.querySelectorAll(
+      //   "yt-formatted-string#text.style-scope.ytd-button-renderer.style-default.size-default"
+      // );
 
-      const thanksButton = document.querySelectorAll(
-        "yt-formatted-string#text.style-scope.ytd-button-renderer.style-default.size-default"
-      );
+      // const thanksButton = document.querySelectorAll(
+      //   "yt-formatted-string#text.style-scope.ytd-button-renderer.style-default.size-default"
+      // );
 
       if (prgroess) {
         return;
@@ -161,21 +153,14 @@ chrome.storage.sync.get("savedApi", ({ savedApi }) => {
       color.style.height = "3px";
       color.setAttribute("id", "color");
 
-      // example video https://www.youtube.com/watch?v=J07l-Qe9xgs
-      if (clipButton || thanksButton) {
-        thanksButton.forEach((data) => {
-          if (data.textContent === "Thanks") progress.style.width = "32.5%";
-        });
-        clipButton.forEach((data) => {
-          if (data.textContent === "Clip") {
-            progress.style.width = "32.5%";
-          }
-        });
+      if (clipButton) {
+        progress.style.width = "32.5%";
+      } else if (ThanksButton) {
+        progress.style.width = "30.5%";
       }
-      if (clipButton && thanksButton) {
-        thanksButton.forEach((data) => {
-          if (data.textContent === "Thanks") progress.style.width = "25.5%";
-        });
+
+      if (clipButton && ThanksButton) {
+        progress.style.width = "25.5%";
       }
 
       progress.appendChild(color);
